a3e92cde0feb21ce81cd22b306ccc4774a44a997
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-05-26 18:52:34 -0400

    Inaugural Pizza Test + Bug Patch
     - Fixed a bug involving the http get request not being formatted properly

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import requests
  Format: https://api.edamam.com/search?q={FOOD_NAME}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}
 '''
 
-food = ""
+food = "pizza"
 app_id = ""
 app_key = ""
 
-edmam_api_path = "https://api.edamam.com/search?q=" + food + "&app_id=" + app_id + "$app_key" + app_key
+edmam_api_path = "https://api.edamam.com/search?q=" + food + "&app_id=" + app_id + "&app_key=" + app_key
 
 recipe_request = requests.get(edmam_api_path)
+
+print(recipe_request)
 
 print("All good!")
