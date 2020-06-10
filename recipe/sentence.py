42852db085b8a35bfbd4964d3f0affbe5e3923c8
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-06-10 17:35:49 -0400

    Added Minimum Length Checks in Sentence.py
     - Created a new instance function that will check whether a sentecne has passed the minimum 3 words required for a sentence

--- recipe/sentence.py
+++ recipe/sentence.py
@@ -2,3 +2,9 @@ class Sentence:
 
     def __init__(self, source_sentence):
         self.sentence = source_sentence.split(" ")
+
+    def in_length(self):
+        if len(self.sentence) > 3:
+            return True
+        else:
+            return False
