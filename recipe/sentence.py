d909efde9b6f4a72c99fa831a72b2015d0e57e11
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-06-11 19:18:26 -0400

    Added Spacy
     - Added Spacy phrase/syntax identification and morphology classification

--- recipe/sentence.py
+++ recipe/sentence.py
@@ -1,13 +1,25 @@
+import spacy
+
 class Sentence:
 
     def __init__(self, source_sentence):
         self.sentence = source_sentence.split(" ")
+        self.dep_types = ["na", "nsubj", "aux", "ROOT", "prep", "pcomp", "compound", "dobj", "quantmod", "pobj"]
 
     def in_length(self):
         if len(self.sentence) > 3:
             return True
         else:
             return False
+    
+    def vectorize(self, nlp):
+        vector = []
+        doc = nlp(self.sentence)
+        for token in doc:
+            vector.append(vector_value(token.dep_))
+
+    def vector_value(self, type):
+        return self.dep_types.index(type)
 
     def print_self(self):
         for word in self.sentence:
