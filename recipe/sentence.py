dbe89b9c5cceb52981e5d2484233cf7c5c679754
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-06-13 14:45:12 -0400

    Added Alternative Tagging Methods for Vectorization
     - Added the UPOS method for part-of-speech tagging in sentence.py

--- recipe/sentence.py
+++ recipe/sentence.py
@@ -4,7 +4,8 @@ class Sentence:
 
     def __init__(self, source_sentence):
         self.sentence = source_sentence.split(" ")
-        self.dep_types = ["na", "nsubj", "aux", "ROOT", "prep", "pcomp", "compound", "dobj", "quantmod", "pobj"]
+        self.dep_types = ["NA", "nsubj", "aux", "ROOT", "prep", "pcomp", "compound", "dobj", "quantmod", "pobj"]
+        self.pos_types = ["PROPN", "VERB", "ADP", "NOUN", "SYM", "NUM"]
 
     def in_length(self):
         if len(self.sentence) > 3:
@@ -12,16 +13,31 @@ class Sentence:
         else:
             return False
     
+    # Default vectorization method: Syntatic dependency
     def vectorize(self, nlp):
         vector = []
         doc = nlp(self.sentence)
         for token in doc:
-            vector.append(vector_value(token.dep_))
+            vector.append(vector_dep_value(token.dep_))
         self.vector = vector
         return vector
 
-    def vector_value(self, type):
+    # Pos vectorization (Simple UPOS part-of-speech tag)
+    def lemma_vectorize(self, nlp):
+        vector = []
+        doc = nlp(self.sentence)
+        for token in doc:
+            vector.append(vector_pos_value(token.pos_))
+        self.vector = vector
+        return vector
+
+    # Tagging methods for vectorization
+
+    def vector_dep_value(self, type):
         return self.dep_types.index(type)
+
+    def vector_pos_value(self, type):
+        return self.pos_types.index(type)
 
     def print_self(self):
         for word in self.sentence:
